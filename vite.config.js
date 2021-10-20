db6d71a175f463e033e08c120ea307f1cd6d3405
Psycarlo <karkosyk@gmail.com>
2021-10-21 00:01:07 +0100

    :v: Vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 
 export default defineConfig({
+  base: '/psyquestions/',
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
